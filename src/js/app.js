b7280ad33fe7f6e049cf071287cdef9a79ff0e8e
drmyersii <davidrmyersii@gmail.com>
2016-03-31 00:50:25 -0400

    Fixing formatting.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,10 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
 
-class App extends React.Component {
-    render() {
+class App extends React.Component
+{
+    render ()
+    {
         return (
             <div className="greeting">
                 <p className="greeting-text">Hello, World!</p>
