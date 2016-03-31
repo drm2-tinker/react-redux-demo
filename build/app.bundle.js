5705bbf0fccb2dc0758e204623e561adcc8a3cf7
drmyersii <davidrmyersii@gmail.com>
2016-03-31 01:19:22 -0400

    Adding new build of current source.

--- build/app.bundle.js
+++ build/app.bundle.js
@@ -54,7 +54,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "b75ebaa0cc4f6dfb61f8"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "e4828d83979c344c4546"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -584,6 +584,10 @@
 
 	var _reactDom2 = _interopRequireDefault(_reactDom);
 
+	var _SearchBar = __webpack_require__(159);
+
+	var _SearchBar2 = _interopRequireDefault(_SearchBar);
+
 	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -606,12 +610,8 @@
 	        value: function render() {
 	            return _react2.default.createElement(
 	                'div',
-	                { className: 'greeting' },
-	                _react2.default.createElement(
-	                    'p',
-	                    { className: 'greeting-text' },
-	                    'Hello, World!'
-	                )
+	                null,
+	                _react2.default.createElement(_SearchBar2.default, null)
 	            );
 	        }
 	    }]);
@@ -20221,6 +20221,59 @@
 
 	module.exports = __webpack_require__(3);
 
+
+/***/ },
+/* 159 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, "__esModule", {
+	    value: true
+	});
+
+	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+	var _react = __webpack_require__(1);
+
+	var _react2 = _interopRequireDefault(_react);
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+	var SearchBar = function (_React$Component) {
+	    _inherits(SearchBar, _React$Component);
+
+	    function SearchBar() {
+	        _classCallCheck(this, SearchBar);
+
+	        return _possibleConstructorReturn(this, Object.getPrototypeOf(SearchBar).apply(this, arguments));
+	    }
+
+	    _createClass(SearchBar, [{
+	        key: 'render',
+	        value: function render() {
+	            return _react2.default.createElement(
+	                'div',
+	                null,
+	                _react2.default.createElement(
+	                    'h1',
+	                    null,
+	                    'Search!'
+	                )
+	            );
+	        }
+	    }]);
+
+	    return SearchBar;
+	}(_react2.default.Component);
+
+	exports.default = SearchBar;
 
 /***/ }
 /******/ ]);
