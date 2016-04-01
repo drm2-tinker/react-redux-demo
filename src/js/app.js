5c63bbb4b9a5f99d2e0b62de27ea19bf18b87c20
drmyersii <davidrmyersii@gmail.com>
2016-04-01 18:09:18 -0400

    Adding Giphy API call to handleTermChange() method.

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import ReactDOM  from 'react-dom';
 import SearchBar from './components/SearchBar';
 import GifList   from './components/GifList';
 
+import request   from 'superagent';
+
 class App extends React.Component
 {
     constructor (props)
@@ -16,7 +18,13 @@ class App extends React.Component
 
     handleTermChange (term)
     {
-        console.log(term);
+        // url for the Giphy API with the public dev API key
+        const url = `https://api.giphy.com/v1/gifs/search?q=${term}&api_key=dc6zaTOxFJmzC`;
+
+        request.get(url, (err, res) =>
+        {
+            console.log(res.body.data[0]);
+        });
     }
 
     render ()
