7dd0da7aab06c8df3690d3d4d92cd851d9b1c78b
drmyersii <davidrmyersii@gmail.com>
2016-04-01 00:43:19 -0400

    Implementing input and using App's passed props.

--- src/js/components/SearchBar.js
+++ src/js/components/SearchBar.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 class SearchBar extends React.Component
 {
+    constructor (props)
+    {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
+    onInputChange (term)
+    {
+        this.setState({
+            term: term
+        });
+
+        this.props.onTermChange(term);
+    }
+
     render ()
     {
         return (
-            <div>
-                <h1>Search!</h1>
+            <div className="search">
+                <input onChange={e => this.onInputChange(e.target.value)} />
             </div>
         );
     }
