5eeacbf3a734dd3e32965b060a127624ca364645
drmyersii <davidrmyersii@gmail.com>
2016-04-01 17:58:50 -0400

    Implementing new GifList and GifItem components. Adding basic hardcoded state for gifs.

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,43 @@
 import React     from 'react';
 import ReactDOM  from 'react-dom';
 import SearchBar from './components/SearchBar';
+import GifList   from './components/GifList';
 
 class App extends React.Component
 {
+    constructor (props)
+    {
+        super(props);
+
+        this.state = {
+            gifs: [
+                {
+                    id: 1,
+                    url: 'http://fakeimg.pl/300/?text=Image+1',
+                },
+                {
+                    id: 2,
+                    url: 'http://fakeimg.pl/300/?text=Image+2',
+                },
+                {
+                    id: 3,
+                    url: 'http://fakeimg.pl/300/?text=Image+3',
+                },
+            ]
+        };
+    }
+
     handleTermChange (term)
     {
         console.log(term);
     }
-    
+
     render ()
     {
         return (
             <div>
                 <SearchBar onTermChange={this.handleTermChange} />
+                <GifList gifs={this.state.gifs} />
             </div>
         );
     }
